0ebb3535fc481965b3a03d61111ddf9582fa556e
hamooo32 <169149891+hamooo32@users.noreply.github.com>
2026-07-28 05:53:57 +0400

    s

--- script.js
+++ script.js
@@ -54,9 +54,7 @@ new Promise((resolve,reject)=>{
         const MaghribTime = document.getElementById("MaghribTime")
         const IshaTime = document.getElementById("IshaTime")
         
-        fetch(`https://api.aladhan.com/v1/timings/${today}?latitude=${latitude}&longitude=${longitude}`,{
-            method:'GET'
-        })
+        fetch(`https://api.aladhan.com/v1/timings/${today}?latitude=${latitude}&longitude=${longitude}`)
         .then(salawat => {
             console.log([...salawat.headers.entries()])
             salawat.json()})
